3589a721c744ce17376e7d281c7d934517ce9fad
Abdoulrazack <41553365+Abdoulrazack95@users.noreply.github.com>
2019-08-13 23:38:36 +0100

    increment method for each counter done

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -18,6 +18,14 @@ class Counters extends Component {
     });
   };
 
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
   handleReset = () => {
     const counters = this.state.counters.map(c => {
       c.value = 0;
@@ -25,12 +33,13 @@ class Counters extends Component {
     });
     this.setState({ counters });
   };
+
   render() {
     return (
       <div>
         <button
           className="btn btn-primary btn-sm m-2"
-          onChange={this.handleReset}
+          onClick={this.handleReset}
         >
           Reset
         </button>
@@ -38,6 +47,7 @@ class Counters extends Component {
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             counter={counter}
           />
         ))}
