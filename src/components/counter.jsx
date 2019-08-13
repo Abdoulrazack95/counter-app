42169dc6d346ddb5012ebb8ef1869821d88218ab
Abdoulrazack <41553365+Abdoulrazack95@users.noreply.github.com>
2019-08-13 14:35:52 +0100

    setting up the first counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return ( 
-            <h1>hello there</h1>
-         );
-    }
+  state = {
+    // it is a javascript object in which we can include any data.
+    count: 0
+  };
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>zero</h1> : count;
+  }
+
+  formatColor() {
+    let classColor = "badge m-2 badge-";
+    classColor += this.state.count === 0 ? "warning" : "primary";
+    return classColor;
+  }
+  render() {
+    return (
+      <div>
+        <span className={this.formatColor()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
 }
- 
+
 export default Counter;
